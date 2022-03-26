91ce8c3cfed9c9df08ef2acacc14da5d12de3eb5
Gabriel Cruz L <gabrielcl45@gmail.com>
2022-03-26 18:50:46 -0500

    Add test for deleting a book

---
+++ cypress/integration/delete-book.spec.js
@@ -0,0 +1,61 @@
+describe("Given I want to delete a book", () => {
+  before(() => {
+    // Arrange
+    cy.visit("http://localhost:4200/dashboard");
+    // cy.get(".ant-btn-primary").contains("Add").click();
+    // cy.wait(300);
+    // cy.get("#name").type("Ulyses");
+    // cy.get("#author").type("James Joyce");
+    // cy.contains("Save").click();
+    cy.wait(300);
+
+    Cypress.Commands.add("clickVisibleButton", () => {
+      cy.get("body").then(($body) => {
+        const bookIsVisible = $body.text().includes("James Joyce");
+        if (!bookIsVisible) {
+          if (
+            $body.find(
+              ".ant-pagination-next.ng-star-inserted.ant-pagination-disabled"
+            ).length
+          ) {
+            return false;
+          }
+          cy.get("button .anticon-right ").click();
+          cy.wait(300);
+          cy.clickVisibleButton();
+        } else {
+          cy.get("tr")
+            .contains("tr", "Ulyses")
+            .within(() => {
+              cy.get('[type="checkbox"]').check();
+            });
+        }
+      });
+    });
+
+    cy.clickVisibleButton();
+
+    // Act
+    cy.get(".ant-btn").contains("Delete").click();
+  });
+
+  it("The book should not exist", () => {
+    // Assert
+    // let foundTheBook = cy.clickVisibleButton();
+    // expect(foundTheBook).to.be.false;
+    // cy.get("body").expect("exist");
+
+    cy.visit("http://localhost:4200/dashboard");
+    cy.wait(300);
+    cy.clickVisibleButton();
+    cy.get("tr").contains("tr", "Ulyses").should("not.exist");
+  });
+
+  after(() => {
+    cy.get(".ant-btn-primary").contains("Add").click();
+    cy.wait(500);
+    cy.get("#name").type("Ulyses");
+    cy.get("#author").type("James Joyce");
+    cy.contains("Save").click();
+  });
+});
