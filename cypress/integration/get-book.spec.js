df3ca9080a323693be5ac68338a61d5af8088f97
Gabriel Cruz L <gabrielcl45@gmail.com>
2022-03-26 14:47:15 -0500

    Add test for obtaining a book

---
+++ cypress/integration/get-book.spec.js
@@ -0,0 +1,60 @@
+describe("Given I want to obtain a book", () => {
+  before(() => {
+    // Arrange
+    cy.visit("http://localhost:4200/dashboard");
+    // cy.get(".ant-btn-primary").contains("Add").click();
+    // cy.wait(300);
+    // cy.get("#name").type("Ulyses");
+    // cy.get("#author").type("James Joyce");
+    // cy.contains("Save").click();
+    cy.wait(300);
+
+    Cypress.Commands.add("clickVisibleButton", () => {
+      cy.get("body").then(($body) => {
+        const bookIsVisible = $body.text().includes("James Joyce");
+        if (!bookIsVisible) {
+          cy.get("button .anticon-right ").click();
+          cy.wait(300);
+          cy.clickVisibleButton();
+        }
+      });
+    });
+
+    cy.clickVisibleButton();
+
+    // Act
+    cy.get("tr")
+      .contains("tr", "Ulyses")
+      .within(() => {
+        cy.get('[type="checkbox"]').check();
+      });
+  });
+
+  it("The book should be visible in the list of books", () => {
+    //Assert
+    cy.get("tr").contains("tr", "Ulyses").should("exist");
+  });
+
+  it("The book should have its checkbox checked", () => {
+    //Assert
+    cy.get("tr")
+      .contains("tr", "Ulyses")
+      .within(() => {
+        cy.get('[type="checkbox"]').should("be.checked");
+      });
+  });
+
+  // after(() => {
+  //     cy.request(
+  //         'DELETE',
+  //         `http://localhost:4200/dashboard/books/Ulyses`);
+  //         // `http://localhost:4200/dashboard/books/${catName}`);
+  // })
+
+  // Para probar si algo es flaky, esto sirve para ejecutarlo 100 veces
+  // Cypress._.times(100, (i) => {
+  //     it(`num ${i + 1} - test the thing conditionally`, () => {
+  //       // do the conditional bits 100 times
+  //     })
+  //   })
+});
