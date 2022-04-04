6a91cd298771ced6e80eb5e68dc8982e8b311018
Gabriel Cruz L <gabrielcl45@gmail.com>
2022-04-03 22:32:26 -0500

    Finish refactoring the register, get and edit book test files

--- cypress/integration/delete-book.spec.js
+++ cypress/integration/delete-book.spec.js
@@ -10,33 +10,6 @@ describe("Given I want to delete a book", () => {
     cy.contains("Save").click();
     cy.wait(300);
 
-    Cypress.Commands.add("clickVisibleButton", () => {
-      cy.get("body").then(($body) => {
-        const bookIsVisible = $body.text().includes("James Joyce");
-        if (!bookIsVisible) {
-          if ( // This conditional is fulfilled when the last page of books has been reached
-            $body.find(
-              ".ant-pagination-next.ng-star-inserted.ant-pagination-disabled"
-            ).length
-          ) {
-            // Since bookIsVisible = False and the last page was reached, the book doesn't exist anymore
-            continue;
-          }
-          // Moves on to the next page
-          cy.get("button .anticon-right ").click();
-          cy.wait(300);
-          cy.clickVisibleButton();
-        } else {
-          // Else, the book was found in the current page. Its checkbox gets checked so that it can later be deleted
-          cy.get("tr")
-            .contains("tr", "Ulyses")
-            .within(() => {
-              cy.get('[type="checkbox"]').check();
-            });
-        }
-      });
-    });
-
     // Finds the book and selects it by checking its checkbox
     cy.clickVisibleButton();
 
@@ -54,5 +27,32 @@ describe("Given I want to delete a book", () => {
 
     // Assert
     cy.get("tr").contains("tr", "Ulyses").should("not.exist");
+  });
+});
+
+Cypress.Commands.add("clickVisibleButton", () => {
+  cy.get("body").then(($body) => {
+    const bookIsVisible = $body.text().includes("James Joyce");
+    if (!bookIsVisible) {
+      if ( // This conditional is fulfilled when the last page of books has been reached
+        $body.find(
+          ".ant-pagination-next.ng-star-inserted.ant-pagination-disabled"
+        ).length
+      ) {
+        // Since bookIsVisible = False and the last page was reached, the book doesn't exist anymore
+        continue;
+      }
+      // Moves on to the next page
+      cy.get("button .anticon-right ").click();
+      cy.wait(300);
+      cy.clickVisibleButton();
+    } else {
+      // Else, the book was found in the current page. Its checkbox gets checked so that it can later be deleted
+      cy.get("tr")
+        .contains("tr", "Ulyses")
+        .within(() => {
+          cy.get('[type="checkbox"]').check();
+        });
+    }
   });
 });
--- cypress/integration/edit-book.spec.js
+++ cypress/integration/edit-book.spec.js
@@ -1,3 +1,72 @@
+let currentBookName = "Ulyses";
+let currentBookAuthor = "James Joyce";
+let newBookName = "Ulises";
+let newBookAuthor = "James Joice";
+describe("Given I want to edit a book", () => {
+  before(() => {
+    // Arrange
+    cy.visit("dashboard");
+    cy.get(".ant-btn-primary").contains("Add").click();
+    cy.wait(300);
+    cy.get("#name").type(currentBookName);
+    cy.get("#author").type(currentBookAuthor);
+    cy.contains("Save").click();
+    cy.wait(500);
+  });
+
+  beforeEach(() => {
+    // Arrange
+    // Go back to the main page
+    cy.get(".ant-pagination-item.ng-star-inserted").contains("1").click();
+  });
+
+  Cypress._.times(10, (i) => {
+    it(`Iteration #${
+      i + 1
+    } - The book's name and author should be modified`, () => {
+      // Act
+      // Get the book and change its properties to the new values
+      cy.getAndEditBook();
+
+      // Assert
+      cy.get("tr").contains("tr", "James Joice").should("exist");
+      cy.get("tr").contains("tr", "Ulises").should("exist");
+
+      // Go back to the main page
+      cy.get(".ant-pagination-item.ng-star-inserted").contains("1").click();
+      cy.wait(300);
+
+      cy.getAndEditBook();
+    });
+  });
+
+  it("The book should not be allowed to be edited with empty values for its name nor for its author's name", () => {
+    // Arrange
+    // Get the book and open the edit form
+    cy.get("tr")
+      .contains("tr", currentBookName)
+      .within(() => {
+        cy.get(".anticon-edit").click();
+      });
+
+    // Act
+    cy.get("#author").type(currentBookAuthor);
+    cy.get("#name").clear();
+
+    // Assert
+    cy.contains("Save").should("be.disabled");
+
+    // Act
+    cy.get("#name").type(currentBookName);
+    cy.get("#author").clear();
+
+    // Assert
+    cy.contains("Save").should("be.disabled");
+
+    cy.contains("Cancel").click();
+  });
+});
+
 Cypress.Commands.add("getAndEditBook", () => {
   cy.get("body").then(($body) => {
     const bookIsVisible = $body.text().includes(currentBookName);
@@ -6,7 +75,7 @@ Cypress.Commands.add("getAndEditBook", () => {
       // This conditional checks if the book exists in the current page. Otherwise, it moves on to the next page
       cy.get("button .anticon-right ").click();
       cy.wait(500);
-      cy.clickVisibleButton();
+      cy.getAndEditBook();
     } else {
       // Else, the book is obtained from its parent tr element and its corresponding edit button is clicked
       cy.get("tr")
@@ -16,52 +85,20 @@ Cypress.Commands.add("getAndEditBook", () => {
         });
 
       cy.wait(300);
+
       // The books's new fields are input
       cy.get("#name").clear().type(newBookName);
       cy.get("#author").clear().type(newBookAuthor);
 
-      currentBookName = "Ulises";
-      newBookName = "Ulyses";
-      newBookAuthor = "James Joyce";
+      // Alternate var values for future function call
+      const tempBookName = currentBookName;
+      const tempBookAuthor = currentBookAuthor;
+      currentBookName = newBookName;
+      currentBookAuthor = newBookAuthor;
+      newBookName = tempBookName;
+      newBookAuthor = tempBookAuthor;
+
+      cy.contains("Save").click();
     }
-  });
-});
-
-let currentBookName = "Ulyses";
-let currentBookAuthor = "James Joyce";
-let newBookName = "Ulises";
-let newBookAuthor = "James Joice";
-describe("Given I want to edit a book", () => {
-  before(() => {
-    // Arrange
-    cy.visit("http://localhost:4200/dashboard");
-    cy.get(".ant-btn-primary").contains("Add").click();
-    cy.wait(300);
-    cy.get("#name").type(currentBookName);
-    cy.get("#author").type(currentBookAuthor);
-    cy.contains("Save").click();
-    cy.wait(500);
-
-    // Gets the book and fills its fields with the new values
-    cy.getAndEditBook();
-
-    // Act
-    cy.contains("Save").click();
-  });
-
-  it("The book's author should be modified", () => {
-    // Assert
-    cy.get("tr").contains("tr", "James Joice").should("exist");
-  });
-
-  it("The book's name should be modified", () => {
-    // Assert
-    cy.get("tr").contains("tr", "Ulises").should("exist");
-  });
-
-  after(() => {
-    // Return the book's values to their original ones
-    cy.getAndEditBook();
-    cy.contains("Save").click();
   });
 });
